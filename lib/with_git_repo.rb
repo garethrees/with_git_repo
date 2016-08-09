e903d77559d5f47f4167e8be69a97b59a12f48f9
Chris Mytton <chrismytton@gmail.com>
2016-08-09 16:38:21 +0100

    Initial commit

---
+++ lib/with_git_repo.rb
@@ -0,0 +1,5 @@
+require 'with_git_repo/version'
+
+class WithGitRepo
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,4 @@
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'with_git_repo'
+
+require 'minitest/autorun'
