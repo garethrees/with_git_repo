71df2e96c6675cbce3f81e36b36623fb9efc938c
Chris Mytton <chrismytton@gmail.com>
2016-08-09 17:27:27 +0100

    Initial implementation

--- lib/with_git_repo.rb
+++ lib/with_git_repo.rb
@@ -1,5 +1,47 @@
 require 'with_git_repo/version'
 
 class WithGitRepo
-  # Your code goes here...
+  attr_reader :clone_url
+  attr_reader :user_name
+  attr_reader :user_email
+
+  def initialize(options = {})
+    @clone_url = options.fetch(:clone_url)
+    @user_name = options.fetch(:user_name, 'with_git_repo')
+    @user_email = options.fetch(:user_email, 'with_git_repo@everypolitician.org')
+  end
+
+  def commit_changes_to_branch(branch, message)
+    if git.branches["origin/#{branch}"]
+      git.checkout(branch)
+    else
+      git.checkout(branch, new_branch: true)
+    end
+    git.chdir { yield }
+    git.add
+    return unless git.status.changed.any? || git.status.added.any?
+    git.commit(message)
+    git.push('origin', branch)
+  end
+
+  private
+
+  def checkout_branch(branch)
+    if git.branches["origin/#{branch}"]
+      git.checkout(branch)
+    else
+      git.checkout(branch, new_branch: true)
+    end
+  end
+
+  def git
+    @git ||= Git.clone(clone_url, '.', path: tmpdir).tap do |g|
+      g.config('user.name', user_name)
+      g.config('user.email', user_email)
+    end
+  end
+
+  def tmpdir
+    @tmpdir ||= Dir.mktmpdir
+  end
 end
