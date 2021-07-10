968d1e06d3ed478988887d798411c49553fb5ff7
Gareth Rees <gareth@garethrees.co.uk>
2021-07-10 20:19:26 +0100

    Add #commit_changes_to_branch test coverage
    There’s a bit too much going on here, but this at least provides a base
    level of coverage for refactoring.
    
    We wrap the specs in an `around` block to take us into a tmpdir where
    we’ve copied our sample git repo. We then clone that into a working copy
    via WithGitRepo and perform our operation (in the spec `subject`). After
    we’ve done that, we need to re-clone the repo (to another tmpdir) so
    that we can check that we did indeed make the commit and push our
    changes.

--- spec/with_git_repo_spec.rb
+++ spec/with_git_repo_spec.rb
@@ -48,4 +48,101 @@ describe WithGitRepo do
       end
     end
   end
+
+  describe '#commit_changes_to_branch' do
+    subject do
+      with_git_repo.commit_changes_to_branch(branch, msg) do
+        File.write('greeting.txt', 'Hello, world!')
+      end
+    end
+
+    let(:clone_url) { "file://#{@dir}/with_git_repo.git" }
+    let(:default_options) { { clone_url: clone_url } }
+    let(:options) { default_options }
+    let(:with_git_repo) { WithGitRepo.new(options) }
+
+    let(:branch) { 'master' }
+    let(:msg) { 'a commit message' }
+
+    around do |test|
+      with_tmpdir do |dir|
+        @dir = dir
+        test.call
+      end
+    end
+
+    before { subject }
+
+    let(:git) do
+      Git.clone(clone_url, 'cloned_repo_after_subject', path: Dir.mktmpdir)
+    end
+
+    it 'the changed file has the correct contents' do
+      blob = git.gcommit('HEAD').gtree.blobs['greeting.txt']
+      assert_equal 'Hello, world!', blob.contents
+    end
+
+    it 'commits the changes with the given message' do
+      assert_equal 'a commit message', git.gcommit('HEAD').message
+    end
+
+    context 'when using the default branch' do
+      it 'pushes the changed file after committing' do
+        refute_nil git.gcommit('HEAD').gtree.blobs['greeting.txt']
+      end
+    end
+
+    context 'when using a non-default branch that does not exist' do
+      let(:branch) { 'new-branch' }
+
+      it 'pushes the changed file to the given branch after committing' do
+        commit = git.branches['origin/new-branch'].gcommit
+        refute_nil commit.gtree.blobs['greeting.txt']
+      end
+    end
+
+    context 'when using a non-default branch that already exists' do
+      let(:branch) { 'existing-branch' }
+
+      before do
+        with_git_repo.commit_changes_to_branch(branch, 'msg') do
+          File.write('before.txt', 'before assert')
+        end
+      end
+
+      it 'pushes the changed file to the given branch after committing' do
+        commit = git.branches['origin/existing-branch'].gcommit
+        refute_nil commit.gtree.blobs['greeting.txt']
+      end
+    end
+
+    context 'with the default user.name' do
+      it 'commits the changes with the default user.name' do
+        assert_equal 'with_git_repo', git.gcommit('HEAD').author.name
+      end
+    end
+
+    context 'with an optional user.name' do
+      let(:options) { default_options.merge(user_name: 'foo') }
+
+      it 'commits the changes with the optional user.name' do
+        assert_equal 'foo', git.gcommit('HEAD').author.name
+      end
+    end
+
+    context 'with the default user.email' do
+      it 'commits the changes with the default user.email' do
+        default_email = 'with_git_repo@everypolitician.org'
+        assert_equal default_email, git.gcommit('HEAD').author.email
+      end
+    end
+
+    context 'with an optional user.email' do
+      let(:options) { default_options.merge(user_email: 'foo@example.com') }
+
+      it 'commits the changes with the optional user.email' do
+        assert_equal 'foo@example.com', git.gcommit('HEAD').author.email
+      end
+    end
+  end
 end
