3fbd45cb96fbb46af914f7522e83eacc8718e600
Gareth Rees <gareth@garethrees.co.uk>
2021-07-10 20:07:21 +0100

    Add spec setup
    * Add a bare repo to use instead of an online remote
    * Add spec helpers to copy the bare repo into a tmpdir so that we start
      each spec run from a known state
    * Add minitest-around so that we have a nicer DSL for accessing the
      tmpdir in assertions

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,26 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'with_git_repo'
 
 require 'minitest/autorun'
+require 'minitest/around/spec'
+require 'fileutils'
+require 'pry'
+
+def with_tmpdir
+  Dir.mktmpdir do |dir|
+    Dir.chdir(dir) do
+      copy_test_repo!
+      yield dir
+    end
+  end
+end
+
+def test_repo_path
+  File.expand_path('../support/with_git_repo.git', __FILE__)
+end
+
+def copy_test_repo!
+  FileUtils.cp_r(test_repo_path, '.')
+end
 
 module MiniTest
   # Monkeypatch to add `context` helper
--- with_git_repo.gemspec
+++ with_git_repo.gemspec
@@ -23,5 +23,6 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'bundler', '~> 2'
   spec.add_development_dependency 'rake', '~> 13.0'
   spec.add_development_dependency 'minitest', '~> 5.0'
+  spec.add_development_dependency 'minitest-around', '~> 0.5'
   spec.add_development_dependency 'pry'
 end
