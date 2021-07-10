d499784f88b24640ca8e3d41f9f2709e7be51a06
Gareth Rees <gareth@garethrees.co.uk>
2021-07-10 17:24:18 +0100

    Add context alias
    `describe` signals that there's a class or method under test, whereas
    `context` signals a particular use of the subject under test (different
    inputs, etc)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,12 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'with_git_repo'
 
 require 'minitest/autorun'
+
+module MiniTest
+  # Monkeypatch to add `context` helper
+  class Spec
+    class << self
+      alias context describe
+    end
+  end
+end
